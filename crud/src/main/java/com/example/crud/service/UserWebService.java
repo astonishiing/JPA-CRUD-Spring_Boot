0dc8b1af0f1a180fb03a4c2b6348696a1b40b020
astonishiing <wcardoso4510@gmail.com>
2025-11-25 15:49:46 -0300

    Adicionando UseWebService

--- crud/src/main/java/com/example/crud/service/UserWebService.java
+++ crud/src/main/java/com/example/crud/service/UserWebService.java
@@ -1,4 +1,34 @@
 package com.example.crud.service;
 
+import com.example.crud.entity.User;
+import com.example.crud.repository.UserRepository;
+
+import java.util.List;
+import java.util.UUID;
+
 public class UserWebService {
+    private final UserRepository userRepository;
+
+    public UserWebService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    public User getById(UUID id) {
+        return userRepository.findById(id)
+                .orElse(null);
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    public void delete(UUID id) {
+        userRepository.deleteById(id);
+    }
+
+
 }
